74cf949782004996a393c732d04f2a50d6cca118
niyonkuru2 <niyonkurusam5@gmail.com>
2025-04-14 12:56:19 +0200

    homepage text

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
    <div>
     <div className="p-8">
-        <h1 className="text-2xl font-bold mb-4 text-center">Dashboard</h1>
+        <h1 className="text-2xl font-bold mb-4 text-center">Home Page</h1>
         {/*to do home page content here */}
       </div>
    </div>
--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -4,7 +4,7 @@ const ResetPassword = () => {
   return (
     <div>
       <div className="p-8">
-        <h1 className="text-2xl font-bold mb-4 text-center">Dashboard</h1>
+        <h1 className="text-2xl font-bold mb-4 text-center">Reset Page</h1>
         {/*to do resetpassword page content here */}
       </div>
     </div>
